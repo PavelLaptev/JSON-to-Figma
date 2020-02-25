865e6ebff0bdec30840047ff23fa1ea88f7da3a3
Pavel Laptev <pal@alteos.com>
2020-02-25 23:23:13 +0100

    additional line for sass added in tsconfig

--- src/app/components/elements/CopyLink/index.tsx
+++ src/app/components/elements/CopyLink/index.tsx
@@ -2,16 +2,24 @@ import * as React from 'react';
 
 import styles from './copylink.module.scss';
 
-const CopyLink = () => {
+interface Props {
+    text?: string;
+}
+
+const CopyLink: React.SFC<Props> = props => {
     const copyToClipboard = (text: string): void => {
         console.log(text);
     };
 
     return (
-        <span className={styles.redSpan} onClick={() => copyToClipboard('sd')}>
-            structture
+        <span className={styles.copyLink} onClick={() => copyToClipboard('sd')}>
+            {props.text}
         </span>
     );
+};
+
+CopyLink.defaultProps = {
+    text: 'Hallo',
 };
 
 export default CopyLink;
