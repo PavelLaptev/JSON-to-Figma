edc05be354f9764e140576a482c7bdca4f5f54da
Pavel <laptev.graphics@gmail.com>
2020-03-03 19:19:27 +0100

    relaunchButtons added but not connected

--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -2,9 +2,10 @@ figma.showUI(__html__, {width: 320, height: 200});
 
 figma.ui.onmessage = msg => {
     if (msg.type === 'copy-to-clipboard') {
-        console.log(msg.link);
         figma.notify(msg.notifyText, {
             timeout: 1500,
         });
     }
 };
+
+figma.currentPage.setRelaunchData({edit: 'sdfsdf', open: 'sdff'});
