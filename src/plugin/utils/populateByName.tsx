0579cae419b3a4cd219e719ce2d00ca7046a1e24
Sten Hougaard <sho@trifork.com>
2021-07-29 15:20:57 +0200

    Initial commit

--- src/plugin/utils/populateByName.tsx
+++ src/plugin/utils/populateByName.tsx
@@ -1,36 +1,53 @@
 import {skipSign} from '../../data/skipSign';
 
-export default function populateByName(selectedLayers, JSONobj, selectedItem) {
+export default function populateByName(selectedLayers, JSONobj, keyFromJSON) {
     let newItem = 0;
+    const temp = keyFromJSON.split('.');
+    const keyName = temp[0];
+    const propToChange = temp[1] ?? null;
+
+    console.log(JSON.stringify(JSONobj));
 
     const loopSelected = arr => {
-        arr.map(item => {
-            if (!item.name.includes(skipSign)) {
-                if (item.name.toUpperCase() === selectedItem.toUpperCase() && item.type === 'TEXT') {
-                    figma.loadFontAsync(item.fontName).then(() => {
+        arr.map(node => {
+            const hasSkipSign = node.name.includes(skipSign);
+            const hasMatchingName = node.name.toUpperCase() === keyName.toUpperCase();
+            if (!hasSkipSign && hasMatchingName) {
+                const propValuesFromJSON = JSONobj[newItem];
+                const newPropValue = propValuesFromJSON[keyFromJSON];
+                console.log(
+                    `${newItem}: The node named "${keyName}" should change prop "${propToChange}" to "${newPropValue}"`,
+                    node
+                );
+                newItem = ++newItem;
+                if (propToChange === 'characters' && node.type === 'TEXT') {
+                    console.log('Changing value to ', newPropValue);
+                    figma.loadFontAsync(node.fontName).then(() => {
                         if (typeof JSONobj[newItem] !== 'undefined') {
-                            item.characters = JSONobj[newItem]?.[selectedItem].toString();
-                            newItem = ++newItem;
+                            node.characters = newPropValue;
                         }
                     });
+                } else {
+                    try {
+                        node[propToChange] = newPropValue;
+                    } catch (e) {
+                        console.log(`Failed to set "${propToChange}"`);
+                    }
                 }
-
-                if (item.name.toUpperCase() === selectedItem.toUpperCase() && item.type !== 'TEXT') {
+                if (node.type !== 'TEXT') {
                     if (JSONobj.hasOwnProperty(newItem)) {
                         figma.ui.postMessage({
                             type: 'image-url',
-                            url: JSONobj[newItem][selectedItem].toString(),
-                            targetID: item.id,
+                            url: JSONobj[newItem][keyFromJSON].toString(),
+                            targetID: node.id,
                         });
-                        newItem = ++newItem;
                     }
                 }
-
-                if (item.children) {
-                    loopSelected(item.children);
-                }
-                return;
             }
+            if (node.children) {
+                loopSelected(node.children);
+            }
+            return;
         });
     };
     loopSelected(selectedLayers);
